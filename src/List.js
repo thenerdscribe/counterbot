679090059b06d2d248f995a9fa8924bb3ac81325
Ryan Morton <morton.ryan92@gmail.com>
2018-11-21 16:55:44 -0700

    Updates

--- src/List.js
+++ src/List.js
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import { getLength } from "./App.js";
 import "./List.css";
 
-export default class List extends Component {
-  render() {
-    const items = this.props.list.map((item, index) => {
-      return (
-        <li key={index}>
-          <a href="#0" key={index} onClick={this.props.onClick}>
-            {item}
-          </a>
-          <ul>
-            <li>
-              <strong>Word Count: </strong>
-              {getLength(item)}
-            </li>
-            <li>
-              <strong>Character Count: </strong>
-              {item.length}
-            </li>
-          </ul>
-        </li>
-      );
-    });
+const List = props => {
+  const items = props.list.map((item, index) => {
     return (
-      <div className="List">
-        <h3>Results</h3>
-        <ul>{items}</ul>
-      </div>
+      <li key={index}>
+        <a href="#0" key={index} onClick={props.onClick}>
+          {item}
+        </a>
+        <ul>
+          <li>
+            <strong>Word Count: </strong>
+            {getLength(item)}
+          </li>
+          <li>
+            <strong>Character Count: </strong>
+            {item.length}
+          </li>
+        </ul>
+      </li>
     );
-  }
-}
+  });
+  return (
+    <div className="List">
+      <h3>Results</h3>
+      <ul>{items}</ul>
+    </div>
+  );
+};
+
+export default List;
