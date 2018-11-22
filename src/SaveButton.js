13d72f39f036baf048aee5e975aa9bfcb2b65149
Ryan Morton <morton.ryan92@gmail.com>
2018-11-21 17:05:16 -0700

    Change to functional components

--- src/SaveButton.js
+++ src/SaveButton.js
@@ -2,24 +2,25 @@ import React, { Component } from "react";
 import { saveAs } from "file-saver/FileSaver";
 import "./TextArea.css";
 
-export default class SaveButton extends Component {
-  handleSave() {
+const SaveButton = props => {
+  handleSave = () => {
     var blob = new Blob([this.props.lines.join("\n")], {
       type: "text/plain;charset=utf-8"
     });
     saveAs(blob, "lines.txt");
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <button
-          onClick={() => {
-            this.handleSave();
-          }}
-        >
-          Save
-        </button>
-      </React.Fragment>
-    );
-  }
-}
+  };
+
+  return (
+    <React.Fragment>
+      <button
+        onClick={() => {
+          this.handleSave();
+        }}
+      >
+        Save
+      </button>
+    </React.Fragment>
+  );
+};
+
+export default SaveButton;
--- src/TextArea.js
+++ src/TextArea.js
@@ -1,25 +1,25 @@
 import React, { Component } from "react";
 import "./TextArea.css";
 
-export default class TextArea extends Component {
-  render() {
-    return (
-      <div className="Inner">
-        <h1>CounterBot {this.props.version}</h1>
-        <h3>Word Count: {this.props.wordCount}</h3>
-        <h3>Character Count: {this.props.characterCount}</h3>
-        <form onSubmit={this.props.handleSubmit}>
-          <input
-            type="text"
-            name="text"
-            value={this.props.value}
-            onChange={this.props.handleChange}
-            tabIndex="0"
-            required
-          />
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
-  }
-}
+const TextArea = props => {
+  return (
+    <div className="Inner">
+      <h1>CounterBot {props.version}</h1>
+      <h3>Word Count: {props.wordCount}</h3>
+      <h3>Character Count: {props.characterCount}</h3>
+      <form onSubmit={props.handleSubmit}>
+        <input
+          type="text"
+          name="text"
+          value={props.value}
+          onChange={props.handleChange}
+          tabIndex="0"
+          required
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default TextArea;
